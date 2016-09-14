cfe4ffef1325f38137ed971a44d3b3878b13eaaa
MaciejKawczynski <sqars89@gmail.com>
2016-09-14 09:41:36 +0200

    add storeConfig

--- app/app.jsx
+++ app/app.jsx
@@ -2,8 +2,18 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
-
 var TodoApp = require('TodoApp')
+
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() =>{
+  console.log('new state', store.getState());
+});
+
+store.dispatch(actions.addTodo('walk dog'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
 
 // load foundation
 $(document).foundation();
--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,7 @@ module.exports = {
       './app/api'
     ],
     alias:{
+      configureStore: 'app/store/configureStore.jsx',
       reducers: 'app/reducers/reducers.jsx',
       actions: 'app/actions/actions.jsx',
       applicationStyles: 'app/styles/app.scss'
