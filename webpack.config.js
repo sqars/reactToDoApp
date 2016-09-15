49a254e9111aee639fdfec8568d3f480276d8db0
MaciejKawczynski <sqars89@gmail.com>
2016-09-15 10:20:24 +0200

    bug fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = JSON.stringify(process.env.NODE_ENV || 'development');
 
 
 module.exports = {
