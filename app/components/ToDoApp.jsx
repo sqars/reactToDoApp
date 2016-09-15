89e066e2720e5fe42c1042578a189db5b49e7e01
MaciejKawczynski <sqars89@gmail.com>
2016-09-15 11:54:10 +0200

    bug fixes 4

--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -10,7 +10,7 @@ var TodoApp = React.createClass({
   render: function(){
     return(
       <div>
-        <h1 className="page-title">Todo App</h1>
+        <h1 className="page-title">Todo Application</h1>
         <div className="row">
           <div className="column small-centered small-11 medium-6 large-5">
             <div className="container">
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
   entry: [
@@ -21,6 +20,9 @@ module.exports = {
       compressor: {
         warnings: false
       }
+    }),
+    new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     })
   ],
   output: {
