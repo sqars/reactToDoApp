37be41e872f5f013e3b5d55cf330d80ec93ef288
MaciejKawczynski <sqars89@gmail.com>
2016-09-15 10:41:27 +0200

    bug fixes 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
 
-process.env.NODE_ENV = JSON.stringify(process.env.NODE_ENV || 'development');
-
-
 module.exports = {
   entry: [
     'script!jquery/dist/jquery.min.js',
@@ -22,6 +19,9 @@ module.exports = {
       compressor: {
         warnings: false
       }
+    }),
+    new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     })
   ],
   output: {
