6180e871203830333acb7f3f058e48190592af4a
MaciejKawczynski <sqars89@gmail.com>
2016-09-24 14:07:29 +0200

    moved firebase configs to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,11 +2,11 @@ import firebase from 'firebase';
 
 try {
   var config = {
-    apiKey: "AIzaSyAm0xp7xnUWKGG_RMDOCTi6GUrtuTxgJ2U",
-    authDomain: "sqars-todo-app.firebaseapp.com",
-    databaseURL: "https://sqars-todo-app.firebaseio.com",
-    storageBucket: "sqars-todo-app.appspot.com",
-    messagingSenderId: "633180397094"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
     };
   firebase.initializeApp(config);
 } catch(e) {
